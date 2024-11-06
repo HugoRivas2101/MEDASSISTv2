49bae78add7b5593ef2870935f340cae9fb913b4
hugo <hugorivas2101@gmail.com>
2024-11-06 15:38:30 -0500

    Add data

--- medAssistProject/src/pages/HomePaciente/components/LeftPaciente.jsx
+++ medAssistProject/src/pages/HomePaciente/components/LeftPaciente.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './LeftPaciente.css';
 import ImagenPerfil from '../assets/imagenperfil.png';
 import Codigobarras from '../assets/CodigoBarra.png';
+import userData from '../assets/data-personal.json';
 function LeftPaciente() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Suponiendo que quieres mostrar el primer usuario
+    const selectedUser = userData.personas[0];
+    setUser(selectedUser);
+  }, []);
+
+
   return (
     <div className="left-paciente">
     <div className="paciente-item1">
@@ -11,16 +22,26 @@ function LeftPaciente() {
       </div>
       <div className='imagen-perfil'>
         <img src={ImagenPerfil} alt="imagen de perfil" style={{ width: '50%', height: '80%' }}/>
-        <div className='nombre-perfil'> ¡ Hola ! <br/> David Martinez </div>
+        <div className='nombre-perfil'><br/>{user ? user.nombres : 'Cargando...'}</div>
       </div>
-      <div className='edad-perfil' style={{ fontWeight: 'bold'}}>09/02/1985 - 32 años</div>
+      <div className='edad-perfil' style={{ fontWeight: 'bold'}}>{user ? `09/02/1985 - ${user.edad} años` : 'Cargando...'}</div>
       </div>
     <div className="paciente-item2">
       <div className='title-datos-personales'>
       DATOS PERSONALES
       </div>
       <div className='content-datos-personales'></div>
-      
+      {user ? (
+            <>
+              <p><strong>DNI:</strong> {user.dni}</p>
+              <p><strong>Teléfono:</strong> {user.telefono}</p>
+              <p><strong>Domicilio:</strong> {user.domicilio}</p>
+              <p><strong>Correo Electrónico:</strong> {user.correo_electronico}</p>
+              <p><strong>Sexo:</strong> {user.sexo}</p>
+            </>
+          ) : (
+            'Cargando datos...'
+          )}
       </div>
     <div className="paciente-item3">
       <img src={Codigobarras} alt="imagen de perfil" style={{ width: '80%', height: '100%' }}/>
--- medAssistProject/src/pages/HomePaciente/components/MainContent.jsx
+++ medAssistProject/src/pages/HomePaciente/components/MainContent.jsx
@@ -1,20 +1,48 @@
-import React, { useState } from 'react';
-import './MainContent.css';
+import React, { useState, useEffect} from "react";
+import "./MainContent.css";
+import userData from '../assets/data-vital.json';
+
 function MainContent() {
+
+  const [signoVital, setSignoVital] = useState([]);
+
+  useEffect(() => {
+    // Cargar datos de signos vitales desde el JSON
+    //setSignosVitales(userData.signos_vitales);
+    setSignoVital(userData.signos_vitales[0]);
+  }, []);
+
+
   return (
     <main className="main-content">
-    <div className="content-item">
-    <div className='title-datos-personales'>
-    MEDICAMENTOS ASIGNADO
+      <div className="content-item">
+        <div className="title-datos-personales">MEDICAMENTOS ASIGNADO</div>
+        <div className="content-datos-personales">
+
+          
+        </div>
       </div>
-      <div className='content-datos-personales'></div>
-     </div>
-    <div className="content-item">
-    <div className='title-datos-personales'>
-    ULTIMOS SIGNOS VITALES(TRIAJE)
+      <div className="content-item">
+        <div className="title-datos-personales">
+          ULTIMOS SIGNOS VITALES(TRIAJE)
+        </div>
+        <div className="content-datos-personales">
+        {signoVital ? (
+            <div className="signo-vital-item">
+              <p><strong>Altura:</strong> {signoVital.altura}</p>
+              <p><strong>Peso:</strong> {signoVital.peso}</p>
+              <p><strong>Índice de Masa Corporal:</strong> {signoVital.masa_corporal}</p>
+              <p><strong>Temperatura:</strong> {signoVital.temperatura}</p>
+              <p><strong>Frecuencia Respiratoria:</strong> {signoVital.frecuencia_respiratoria}</p>
+              <p><strong>Presión Arterial:</strong> {signoVital.presion_arterial}</p>
+              <p><strong>Frecuencia Cardíaca:</strong> {signoVital.frecuencia_cardiaca}</p>
+            </div>
+          ) : (
+            <p>No hay datos de signos vitales disponibles.</p>
+          )}
+        </div>
       </div>
-      <div className='content-datos-personales'></div></div>
-  </main>
+    </main>
   );
 }
 
